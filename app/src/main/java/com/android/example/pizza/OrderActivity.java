e886fca3d8ed2be021676015b598b80d2548642a
robbier-sozo <sozonovalexander13@gmail.com>
2020-06-24 21:01:04 +0300

    FAB+SNACKBAR
    Signed-off-by: robbier-sozo <sozonovalexander13@gmail.com>

--- app/src/main/java/com/android/example/pizza/OrderActivity.java
+++ app/src/main/java/com/android/example/pizza/OrderActivity.java
@@ -3,8 +3,13 @@ package com.android.example.pizza;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -21,5 +26,29 @@ public class OrderActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
 
+
+    }
+
+
+    public void onClickButton(View view) {
+        startSnackbar();
+    }
+
+
+    private void startSnackbar() {
+        String text = "Hello, im snack-snack!";
+        int duration = Snackbar.LENGTH_SHORT;
+        CoordinatorLayout coordinator = findViewById(R.id.coordinator);
+        Snackbar snackbar = Snackbar.make(coordinator,text,duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code
+
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        snackbar.show();
     }
 }
